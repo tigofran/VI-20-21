f877719c92c8ee96b6047389ff697706b96a4533
Tiago Afonso <tigofran@gmail.com>
2020-11-16 10:02:33 +0000

    Double filter with ordered value

--- heatmap.js
+++ heatmap.js
@@ -6,31 +6,54 @@ data = d3.csv(file, function(d) {
     season : d['Season'],
 	episode : d['Episode'],
 	killer : d['Killer'],
-	name : d['Name']
+	name : d['Name'],
+	killershouse : d['Killers House'],
+	method : d['Method'],
+	gender : d['Gender'],
+	nobility : d['Nobility'],
+	isAnimal : d['Is Animal']
   };
 }).then(function(data) {
-
-	var filterGroups = ['Jon Snow', 'Daenerys Targaryen']
+	var filterGroups = ['Default','Season','Books','Character','Killer\'s House', 'Killing Method',
+						'Gender','Nobility','Animals']
 	var totalByEpisode = [];
-
+	var selectedGroup;
+	var selectedSecond;
 	// add the options to the button
 	var dropdown = d3.select("#selectButton")
-	.selectAll('myOptions')
-		.data(filterGroups)
+	.selectAll()
+	.data(filterGroups)
 	.enter()
 	.append('option')
 	.text(function (d) { return d; }) // text showed in the menu
 	.attr("value", function (d) { return d; }) // corresponding value returned by the button
 
-	// make a nested mapping to store the data
-	//group by airport code, then set populate the mapping above with the total flights
+	var seasonData = d3.map(data, function(d){return d.season;})
+	var seasonValues = Array.from([...new Set(seasonData)]).sort();
+	var bookValues = ['GoT','CoK','SoS','FfC','DwD']
+	var characterData = d3.map(data, function(d){return d.name;})
+	var characterValues = Array.from([...new Set(characterData)]).sort();
+	var killersHouseData = d3.map(data, function(d){return d.killershouse;})
+	var killershouseValues = Array.from([...new Set(killersHouseData)]).sort();
+	var methodData = d3.map(data, function(d){return d.method;})
+	var methodValues = Array.from([...new Set(methodData)]).sort();
+	var genderValues = ['Female', 'Male'];
+	var nobilityValues = ['Noble','Peasant'];
+	var animalData = d3.map(data.filter(function(d) {return d.isAnimal == 1}), function(d){return d.name;})
+	var animalValues = Array.from([...new Set(animalData)]).sort();
+
+	
+	var secondDropdown = d3.select('#secondSelectButton')
+	.style('visibility','hidden');
+
+
 	function createGroups(fil){
 		totalByEpisode = [];
 		if (fil == null)
 			epGroups = d3.group(data, d => d.season, d => d.episode)
 		else
 		epGroups = d3.group(data.filter(fil), d => d.season, d => d.episode)
-		var currentLocationIndex = 0;
+		var currentLocationIndex = 0
 		console.log(epGroups)
 		epGroups.forEach( function (vals, key) {
 			summary_vals = vals.forEach( function (val2, key2) {
@@ -67,12 +90,54 @@ data = d3.csv(file, function(d) {
     // When the button is changed, run the updateChart function
     d3.select("#selectButton").on("change", function(d) {
 		// recover the option that has been chosen
-		console.log("pressed button");
-		var selectedOption = d3.select(this).property("value");
-		console.log(selectedOption);
+		secondDropdown.selectAll('*').remove()
+		selectedGroup = d3.select(this).property("value");
+		if (selectedGroup == 'Default')
+			selectedGroup = undefined;
+		d3.select('#secondSelectButton').style('visibility',function(d) {
+			if (selectedGroup != undefined) return 'visible'; else return 'hidden';})
+			.selectAll()
+			.data(function() {
+				switch(selectedGroup){
+					case 'Season':
+						return seasonValues;
+						break;
+					case 'Books':
+						return bookValues;
+						break;
+					case 'Character':
+						return characterValues;
+						break;
+					case 'Killer\'s House':
+						return killershouseValues;
+						break;
+					case 'Killing Method':
+						return methodValues;
+						break;
+					case 'Gender':
+						return genderValues;
+						break;
+					case 'Nobility':
+						return nobilityValues;
+						break;
+					case 'Animals':
+						return animalValues;
+						break;
+				}
+
+			})
+			.enter()
+			.append('option')
+			.text(function (d) { return d; }) // text showed in the menu
+			.attr("value", function (d) { return d; }) // corresponding value returned by the button
         // run the updateChart function with this selected option
-        updateHeatmap(selectedOption)
-    })
+        updateHeatmap(selectedGroup)
+	})
+	
+	d3.select("#secondSelectButton").on("change", function(d) {
+		// recover the option that has been chosen
+		selectedSecond = d3.select(this).property("value");
+	})
 
 	// Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
 	var seasons = ['1','2','3','4','5','6','7','8']
@@ -236,7 +301,7 @@ data = d3.csv(file, function(d) {
 
 		// Create new data with the selection?
 		var dataFilter = createGroups(function(d){ return d.killer == selectedGroup;});
-		console.log(dataFilter)
+		//console.log(dataFilter)
 		// Give these new data to update line
 		heatmap
 			.data(dataFilter)
@@ -254,8 +319,8 @@ data = d3.csv(file, function(d) {
 			.style("stroke-width", 4)
 			.style("stroke", "none")
 			.style("opacity", 0.8)
-			.on("mouseover",mouseover)
-			.on("mousemove", mousemove)
-			.on("mouseleave", mouseleave)
+			// .on("mouseover",mouseover)
+			// .on("mousemove", mousemove)
+			// .on("mouseleave", mouseleave)
 	  }
 });
